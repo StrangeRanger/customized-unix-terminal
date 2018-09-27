153b1d3d061c79ab948e87b0d734962f48d72493
StrangeRanger <hunterjthompson@comcast.net>
2018-09-26 17:57:04 -0700

    removed vimrc directory becuase by installing/overriding current vim, there is no need for a file to say 'syntax on'. The I removed vimrc from the automatic setup.sh file

--- bash_profile automatic setup.sh
+++ bash_profile automatic setup.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 echo Moving bash_profile to home directory
-cd terminal\ syntax\ highlighting\ and\ aliases
+cd ~/programs/mac-terminal-style-config/terminal\ syntax\ highlighting\ and\ aliases/
 cp bash_profile ~/.bash_profile
 echo Successfully moved bash_profile
-cd ..
-
-echo Moving vimrc to home directory
-cd vimrc
-cp vimrc ~/.vimrc
-echo Successfully moved vimrc
-cd ~/
 
 echo " "
 echo "To make the changes active in your current session execute 'source ~/.bash_profile && source ~/.vimrc' or close your current terminal session and open another"
