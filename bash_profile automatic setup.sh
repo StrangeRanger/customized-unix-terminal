0af7a9a93c84f1a4bec61bc3a40aa494373eb886
StrangeRanger <hunterjthompson@comcast.net>
2018-09-26 18:02:48 -0700

    removed some text from a line

--- bash_profile automatic setup.sh
+++ bash_profile automatic setup.sh
@@ -5,5 +5,4 @@ cp bash_profile ~/.bash_profile
 echo Successfully moved bash_profile
 
 echo " "
-echo "To make the changes active in your current session execute 'source ~/.bash_profile && source ~/.vimrc' or close your current terminal session and open another"
-echo "If any errors such as '-bash: syntax: command not found', just ignore them"
+echo "To make the changes active in your current session execute 'source ~/.bash_profile' or close your current terminal session and open another"
