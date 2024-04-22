07874ec03b021f7456f653d3f097395a6bb3506e
Hunter T <strangeranger@hthompson.dev>
2024-04-21 22:29:19 -0700

    Update .zshrc for macOS

--- includes/zshrc-files/zshrc-macos.zsh
+++ includes/zshrc-files/zshrc-macos.zsh
@@ -1,6 +1,5 @@
-# Fig pre block. Keep at the top of this file.
-[[ -f "$HOME/.fig/shell/zshrc.pre.zsh" ]] && . "$HOME/.fig/shell/zshrc.pre.zsh"
-
+# CodeWhisperer pre block. Keep at the top of this file.
+[[ -f "${HOME}/Library/Application Support/codewhisperer/shell/zshrc.pre.zsh" ]] && builtin source "${HOME}/Library/Application Support/codewhisperer/shell/zshrc.pre.zsh"
 # If PowerLevel10K's Instant Prompt is enabled, `export GPG_TTY=$(tty)` needs to be
 # placed at the top of this file. Find more information here:
 # https://unix.stackexchange.com/questions/608842/zshrc-export-gpg-tty-tty-says-not-a-tty/608921#608921
@@ -48,91 +47,69 @@ HIST_STAMPS="yyyy-mm-dd"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git colored-man-pages copybuffer copypath copyfile bgnotify)
+plugins=(colored-man-pages copybuffer copypath copyfile bgnotify)
 
-# Zsh "plugin" installed via git and the following command:
-# git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
-fpath+=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src
+# ???
+FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
 
 source $ZSH/oh-my-zsh.sh
 
-# User Configurations
 
-########################################################################################
-#### [ General Aliases ]
-
-########################################################################################
-#### [[ Group 1 ]]
+####[ Personal Configurations ]#########################################################
+####[[ General Aliases ]]###############################################################
+####[[[ Group 1 ]]]#####################################################################
 
 
-alias dryupdatezshplugins="bash ~/Programs/mass-git/mass-git -p ~/.oh-my-zsh/custom/plugins/ -r -d"
-alias updatezshplugins="bash ~/Programs/mass-git/mass-git -p ~/.oh-my-zsh/custom/plugins/ -r"
-alias updatebrew="brew update && brew upgrade && brew autoremove && brew cleanup && brew doctor"
+## General aliases.
 alias ic="cd ~/Library/Mobile\ Documents/com~apple~CloudDocs"
 alias edisk="cd /Volumes && ll"
 alias dll="lsd -lh"
 alias dl="lsd -lah"
+alias remove_ds_store="find . -name '*.DS_Store' -type f -delete"
+alias code="open -a 'Visual Studio Code.app' ."
+alias clang_format_recursive="find . -name '*.cs' -type f -exec clang-format --style=file:/Users/hunter/Programs/Mine/Formatter\ Configs/CSharp_clang-format/_clang-format -i {} +"
 
-## GNU command aliases
+## Update based aliases.
+alias updatezshplugins="bash ~/Programs/mass-git/mass-git -p ~/.oh-my-zsh/custom/plugins/ -r"
+alias updatecopilot="bash ~/Programs/mass-git/mass-git -p ~/.config/nvim/pack/github/ -r"
+alias updatebrew="brew update && brew upgrade && brew autoremove && brew cleanup && brew doctor"
+alias updateomz="omz update"
+alias updateall="updatebrew; updateomz; updatecopilot; updatezshplugins"
+
+## GNU command aliases.
 alias cp="gcp"
 alias mv="gmv"
 
 
-#### End of [[ Group 1 ]]
-########################################################################################
-#### [[ Group 2 ]]
-#### Due to the sheer number of commands, I find it difficult to remember them all. So
-#### the aliases below are contains commands that I find difficult to remember.
+####[[[ Group 2 ]]]#####################################################################
+#### Due to the number of commands that I find to be useful, I've created aliases
+#### containing some of these commands. They are specifically commands that I don't
+#### often use, but are useful to have on hand. Having these aliases allows me to see
+#### a list of these commands, without having to commit them to memeory.
 
+alias lt="echo -e \"
+####[ Package Manager installed commands ]##############################################
 
-alias listtools="echo -e \"
-########################################################################################
-#### [ Package Manager installed commands ]
-
-asciinema  - Terminal session recorder.
 bandwhich  - Terminal bandwidth utilization tool.
-bat        - Clone of cat(1) with syntax highlighting and Git integration.
+bat        - A cat(1) clone with wings.
 codespell  - Check code for common misspellings.
 duf        - Disk Usage/Free Utility - a better 'df' alternative.
-fd         - A simple, fast and user-friendly alternative to find.
-gh         - GitHub's official command line tool.
-grex       - Generates regular expressions from user-provided test cases.
-http       - User-friendly cURL replacement (command-line HTTP client).
-m          - Swiss Army Knife for macOS.
-mas        - Mac App Store command line interface.
-muffet     - Fast website link checker in Go.
+fzf        - A command-line fuzzy finder.
 ncdu       - ncdu (NCurses Disk Usage) is a curses-based version of the well-known 'du'.
-pipenv     - Python dependency management tool.
-pipx       - Install and Run Python Applications in Isolated Environments.
-pyenv      - Simple Python version management.
 pstree     - List processes as a tree.
-tldr       - Simplified and community-driven man pages.
-youtube-dl - Command-line program to download videos from YouTube.com and other video
-             sites.
+tmux       - Terminal multiplexer.
 
-########################################################################################
-#### [[ Grouped commands ]]
+####[[ Grouped commands ]]##############################################################
 
-listtools_formatters - List of code formatters and linters.
-listtools_conversion - List of programs used for converting the formats of videos,
-                       images, etc.
+lt_conversion - List of programs used for converting the formats of videos, images, etc.
+lt_git        - List of programs used for git related commands.
 
 
-########################################################################################
-#### [ Keyboard combinations ]
+####[ Keyboard combinations ]###########################################################
 
 Ctrl + O - Allows you to copy what you are currently typing, via 'Ctrl' + 'O'.
 \""
-alias listtools_formatters="echo -e \"
-########################################################################################
-#### [ Code formatters and linters ]
-
-black      - The uncompromising Python code formatter.
-prettier   - Code formatter for JavaScript, CSS, JSON, GraphQL, Markdown, YAML.
-pylint     - It's not just a linter that annoys you!
-shellcheck - Shell script analysis tool.
-\""
-alias listtools_conversion="echo -e \"
+alias lt_conversion="echo -e \"
 ########################################################################################
 #### [ Image and video formatters ]
 
@@ -140,15 +117,38 @@ ffmpeg - FFmpeg is a collection of libraries and tools to process multimedia con
 magick - Convert between image formats as well as resize an image, blur, crop,
          despeckle, dither, draw on, flip, join, re-sample, and much more.
 \""
+alias lt_git="echo -e \"
+####[ Git Related Commands ]###########################################################
+
+lazygit  - Simple terminal UI for git commands.
+git open - Opens the GitHub page for a repo/branch in your browser.
+ugit     - ugit helps you undo git commands without much effort.
+\""
 
 
-#### End of [[ Group 2]]
-########################################################################################
+####[[[ SSH related commands ]]]########################################################
 
-#### End of [ General Aliases ]
-########################################################################################
-#### [ Environmental Variables ]
 
+# SSH related aliases.
+
+
+####[[ Environmental Variables ]]#######################################################
+
+
+# 1Password auth socket.
+export SSH_AUTH_SOCK="~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+
+## NVM setup...
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+# Path purpose:
+#   /usr/local/opt/curl/bin:       ...
+#   /usr/local/sbin:               ???
+#   /usr/local/opt/openjdk@17/bin: Prefered java version.
+#   /usr/local/opt/node@18/bin:    Prefered node version.
+export PATH="/usr/local/opt/curl/bin:/usr/local/sbin:/usr/local/opt/openjdk@17/bin:/usr/local/opt/node@18/bin:$PATH"
 
 # Modifies colors of files and directories when using `ls`.
 export LSCOLORS="exgxfxDxcxegDaabagacaD"
@@ -158,27 +158,43 @@ export LSCOLORS="exgxfxDxcxegDaabagacaD"
 export LS_COLORS="di=34:ln=36:so=35:pi=1;33:ex=32:bd=34;46:cd=1;33;40:su=30;41:sg=30;46:tw=30;42:ow=30;1;43"
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
+## Editor configurations.
+export EDITOR=nvim
+export VISUAL=$EDITOR
 
-#### End of [ Environmental Variables ]
-########################################################################################
-#### [ Sourced Files ]
+
+####[[ Sourced Files ]]#################################################################
 
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 # Zsh "plugin" installed via git and the following command:
-# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+zsh_syntax_highlighting="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+[[ -f $zsh_syntax_highlighting ]] && source "$zsh_syntax_highlighting"
 
 # Zsh "plugin" installed via git and the following command:
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+zsh_autosuggestions="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
+[[ -f $zsh_autosuggestions ]] && source "$zsh_autosuggestions"
+
+# Zsh "plugin" installed via git and the following command:
+# git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+fzf_tab="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab/fzf-tab.plugin.zsh"
+[[ -f $fzf_tab ]] && source "$fzf_tab"
+
+# Enable the use of '1password-cli' plugins.
+source /Users/hunter/.config/op/plugins.sh
+
+## Azure Auto Completion.
+autoload bashcompinit && bashcompinit
+source $(brew --prefix)/etc/bash_completion.d/az
 
 
-#### End of [ Sourced Files ]
-########################################################################################
+####[[ Others ]]########################################################################
+#### These are generally configurations set up by setup scripts or other programs.
 
 
-# Fig post block. Keep at the bottom of this file.
-[[ -f "$HOME/.fig/shell/zshrc.post.zsh" ]] && . "$HOME/.fig/shell/zshrc.post.zsh"
+# CodeWhisperer post block. Keep at the bottom of this file.
+[[ -f "${HOME}/Library/Application Support/codewhisperer/shell/zshrc.post.zsh" ]] && builtin source "${HOME}/Library/Application Support/codewhisperer/shell/zshrc.post.zsh"
