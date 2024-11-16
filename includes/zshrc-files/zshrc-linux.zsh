048bd1cd13c1ac8977455ee1d84d8a74b2400893
Hunter T. <strangeranger@hthompson.dev>
2024-11-15 17:26:43 -0800

    Update non-snippet files

--- includes/zshrc-files/zshrc-linux.zsh
+++ includes/zshrc-files/zshrc-linux.zsh
@@ -1,8 +1,3 @@
-# If PowerLevel10K's Instant Prompt is enabled, `export GPG_TTY=$(tty)` needs to be
-# placed at the top of this file. Find more information here:
-# https://unix.stackexchange.com/questions/608842/zshrc-export-gpg-tty-tty-says-not-a-tty/608921#608921
-export GPG_TTY=$(tty)
-
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
@@ -17,7 +12,7 @@ export ZSH="$HOME/.oh-my-zsh"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="powerlevel10k/powerlevel10k"
+#ZSH_THEME=""
 
 # Uncomment the following line to use case-sensitive completion.
 # CASE_SENSITIVE="true"
@@ -45,8 +40,7 @@ HIST_STAMPS="yyyy-mm-dd"
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-#plugins=(git colored-man-pages copybuffer copypath copyfile bgnotify)  # Desktop
-plugins=(git colored-man-pages)  # Server
+plugins=(colored-man-pages copybuffer copypath copyfile bgnotify command-not-found)  # Desktop
 
 # Zsh "plugin" installed via git and the following command:
 # git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
@@ -58,25 +52,29 @@ source "$ZSH/oh-my-zsh.sh"
 
 ####[ Personal Configurations ]#########################################################
 ####[[ Aliases ]]#######################################################################
-####[[[ Group 1 ]]]#####################################################################
 
+
+###
+### [ Group 1 ]
+###
 
 ## General aliases.
 alias zls="eza"
+alias formatc="find . -name '*.cs' -type f -exec clang-format --style='file:$HOME/Programs/Mine/Formatter Configs/CSharp_clang-format/_clang-format' -i {} +"
+hash xdg-open 2>/dev/null && alias open="xdg-open"
 
 ## Update based aliases.
-alias updatezshplugins="bash ~/Programs/mass-git/mass-git -p ~/.oh-my-zsh/custom/plugins/ -r"
-alias updatecopilot="bash ~/Programs/mass-git/mass-git -p ~/.config/nvim/pack/github/ -r"
-alias updateapt="sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y"
-alias updateomz="omz update"
-alias updateall="updateapt; updateomz; updatecopilot; updatezshplugins"
+alias updateapt="sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean"
+alias updatepacman="sudo pacman -Syu && yay && yay -Yc"
 
-
-####[[[ Group 2 ]]]#####################################################################
-#### Due to the number of commands that I find to be useful, I've created aliases
-#### containing some of these commands. They are specifically commands that I don't
-#### often use, but are useful to have on hand. Having these aliases allows me to see
-#### a list of these commands, without having to commit them to memeory.
+###
+### [ Group 2 ]
+###
+### Due to the number of commands that I find to be useful, I've created aliases
+### containing some of these commands. They are specifically commands that I don't
+### often use, but are useful to have on hand. Having these aliases allows me to see
+### a list of these commands, without having to commit them to memeory.
+###
 
 alias lt="echo -e \"
 ####[ Package Manager installed commands ]##############################################
@@ -121,20 +119,32 @@ ugit     - ugit helps you undo git commands without much effort.
 ####[[ Environmental Variables ]]#######################################################
 
 
-## Modifies the colors of files and directories in the terminal.
+# Modifies the colors of files and directories in the terminal.
 export LS_COLORS="di=34:ln=36:so=35:pi=1;33:ex=32:bd=34;46:cd=1;33;40:su=30;41:sg=30;46:tw=30;42:ow=30;1;43"
-# This is set in the Zsh Style Configurations section.
-#zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
 # PATH value...
-export PATH="$PATH:$HOME/.local/bin"
+export PATH="$PATH:$HOME/.local/bin:/opt/nvim-linux64/bin"
+
+## Set default editor.
+if hash nvim 2>/dev/null; then
+    export EDITOR=nvim
+    export VISUAL=$EDITOR
+fi
+
+# Node Version Manager (NVM) configurations.
+export NVM_DIR="$HOME/.nvm"
 
 
 ####[[ Sourced Files ]]#################################################################
 
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+## Load NVM.
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+## Source the plugins.sh file for the `op` command.
+[[ -f $HOME/.config/op/plugins.sh ]] \
+    && source "$HOME/.config/op/plugins.sh"
 
 # Zsh "plugin" installed via git and the following command:
 # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
@@ -164,10 +174,22 @@ zstyle ':completion:*:descriptions' format '[%d]'
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 ## Preview directory's content with `eza` when completing `cd`.
 hash eza 2>/dev/null \
-&& zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
-# Wwitch group using `<` and `>`.
+    && zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+# Switch group using `<` and `>`.
 zstyle ':fzf-tab:*' switch-group '<' '>'
+
+
+####[[ End of File Configurations ]]####################################################
+#### These are configurations that are specified to be placed at the end of the file, by
+#### the developer/documentation.
+
+
+# Initialize Starship prompt, if it is installed and $ZSH_THEME is not set.
+hash starship 2>/dev/null \
+    && [[ -z $ZSH_THEME ]] \
+    && eval "$(starship init zsh)"
 
 
 ####[[ Others ]]########################################################################
 #### These are generally configurations set up by setup scripts or other programs.
+
--- includes/zshrc-files/zshrc-macos.zsh
+++ includes/zshrc-files/zshrc-macos.zsh
@@ -1,10 +1,3 @@
-# CodeWhisperer pre block. Keep at the top of this file.
-[[ -f "${HOME}/Library/Application Support/codewhisperer/shell/zshrc.pre.zsh" ]] && builtin source "${HOME}/Library/Application Support/codewhisperer/shell/zshrc.pre.zsh"
-# If PowerLevel10K's Instant Prompt is enabled, `export GPG_TTY=$(tty)` needs to be
-# placed at the top of this file. Find more information here:
-# https://unix.stackexchange.com/questions/608842/zshrc-export-gpg-tty-tty-says-not-a-tty/608921#608921
-export GPG_TTY=$(tty)
-
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
@@ -19,7 +12,7 @@ export ZSH="$HOME/.oh-my-zsh"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="powerlevel10k/powerlevel10k"
+#ZSH_THEME=""
 
 # Uncomment the following line to use case-sensitive completion.
 # CASE_SENSITIVE="true"
@@ -34,6 +27,9 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 # Uncomment the following line to enable command auto-correction.
 # ENABLE_CORRECTION="true"
 
+# Disable audo updates for ohmyzsh. This is taken care of by chezmoi.
+DISABLE_AUTO_UPDATE="true"
+
 # Uncomment the following line if you want to change the command execution time
 # stamp shown in the history command output.
 # You can set one of the optional three formats:
@@ -47,6 +43,7 @@ HIST_STAMPS="yyyy-mm-dd"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
+# MAYBE: Add `command-not-found` plugin.
 plugins=(colored-man-pages copybuffer copypath copyfile bgnotify)
 
 # Zsh "plugin" installed via git and the following command:
@@ -59,8 +56,11 @@ source "$ZSH/oh-my-zsh.sh"
 
 ####[ Personal Configurations ]#########################################################
 ####[[ Aliases ]]#######################################################################
-####[[[ Group 1 ]]]#####################################################################
 
+
+###
+### [ Group 1 ]
+###
 
 ## General aliases.
 alias ic="cd ~/Library/Mobile\ Documents/com~apple~CloudDocs"
@@ -68,37 +68,29 @@ alias edisk="cd /Volumes && ll"
 alias zls="eza"
 alias rmdsstore="find . -name '*.DS_Store' -type f -delete"
 alias code="open -a 'Visual Studio Code.app' ."
-alias formatc="find . -name '*.cs' -type f -exec clang-format --style=file:~/Programs/Mine/Formatter\ Configs/CSharp_clang-format/_clang-format -i {} +"
-
-## Update based aliases.
-alias updatezshplugins="bash ~/Programs/mass-git/mass-git -p ~/.oh-my-zsh/custom/plugins/ -r"
-alias updatecopilot="bash ~/Programs/mass-git/mass-git -p ~/.config/nvim/pack/github/ -r"
+alias formatc="find . -name '*.cs' -type f -exec clang-format --style='file:$HOME/Programs/Mine/Formatter Configs/CSharp_clang-format/_clang-format' -i {} +"
 alias updatebrew="brew update && brew upgrade && brew autoremove && brew cleanup && brew doctor"
-alias updateomz="omz update"
-alias updateall="updatebrew; updateomz; updatecopilot; updatezshplugins"
 
-## GNU command aliases.
-alias cp="gcp"
-alias mv="gmv"
-
-
-####[[[ Group 2 ]]]#####################################################################
-#### Due to the number of commands that I find to be useful, I've created aliases
-#### containing some of these commands. They are specifically commands that I don't
-#### often use, but are useful to have on hand. Having these aliases allows me to see
-#### a list of these commands, without having to commit them to memeory.
+###
+### [ Group 2 ]
+###
+### Due to the number of commands that I find to be useful, I've created aliases
+### containing some of these commands. They are specifically commands that I don't
+### often use, but are useful to have on hand. Having these aliases allows me to see
+### a list of these commands, without having to commit them to memeory.
+###
 
 alias lt="echo -e \"
 ####[ Package Manager installed commands ]##############################################
 
-bandwhich  - Terminal bandwidth utilization tool.
-bat        - A cat(1) clone with wings.
-codespell  - Check code for common misspellings.
-duf        - Disk Usage/Free Utility - a better 'df' alternative.
-fzf        - A command-line fuzzy finder.
-ncdu       - ncdu (NCurses Disk Usage) is a curses-based version of the well-known 'du'.
-pstree     - List processes as a tree.
-tmux       - Terminal multiplexer.
+bandwhich - Terminal bandwidth utilization tool.
+bat       - A cat(1) clone with wings.
+codespell - Check code for common misspellings.
+duf       - Disk Usage/Free Utility - a better 'df' alternative.
+fzf       - A command-line fuzzy finder.
+ncdu      - ncdu (NCurses Disk Usage) is a curses-based version of the well-known 'du'.
+pstree    - List processes as a tree.
+tmux      - Terminal multiplexer.
 
 ####[[ Grouped commands ]]##############################################################
 
@@ -127,30 +119,19 @@ ugit     - ugit helps you undo git commands without much effort.
 \""
 
 
-####[[[ SSH related commands ]]]########################################################
-
-
-alias hthompson="ssh -p 16449 hunter@143.198.55.59"
-alias voidtech="ssh ranger@135.181.75.52"
-
-
 ####[[ Environmental Variables ]]#######################################################
 
 
 # 1Password auth socket.
 export SSH_AUTH_SOCK="$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
 
-## NVM setup...
-export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-
 # Path purpose:
 #   /usr/local/opt/curl/bin:       ...
 #   /usr/local/sbin:               ???
 #   /usr/local/opt/openjdk@17/bin: Prefered java version.
 #   /usr/local/opt/node@18/bin:    Prefered node version.
-export PATH="/usr/local/opt/curl/bin:/usr/local/sbin:/usr/local/opt/openjdk@17/bin:/usr/local/opt/node@18/bin:$PATH"
+#   /usr/local/opt/ruby/bin:       Prefered ruby version.
+export PATH="/usr/local/opt/curl/bin:/usr/local/sbin:/usr/local/opt/openjdk@17/bin:/usr/local/opt/node@18/bin:/usr/local/opt/ruby/bin:$PATH"
 
 # Modifies colors of files and directories when using `ls`.
 export LSCOLORS="exgxfxDxcxegDaabagacaD"
@@ -159,29 +140,35 @@ export LSCOLORS="exgxfxDxcxegDaabagacaD"
 ## here: https://github.com/ohmyzsh/ohmyzsh/issues/6060#issuecomment-327934559
 export LS_COLORS="di=34:ln=36:so=35:pi=1;33:ex=32:bd=34;46:cd=1;33;40:su=30;41:sg=30;46:tw=30;42:ow=30;1;43"
 
-## Editor configurations.
-export EDITOR=nvim
-export VISUAL=$EDITOR
+## Set default editor.
+if hash nvim 2>/dev/null; then
+    export EDITOR=nvim
+    export VISUAL=$EDITOR
+fi
+
+# Node Version Manager (NVM) configurations.
+export NVM_DIR="$HOME/.nvm"
 
 
 ####[[ Sourced Files ]]#################################################################
 
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+## Load NVM.
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-# Zsh "plugin" installed via git and the following command:
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+## Zsh "plugin" installed via git and the following command:
+## git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 zsh_syntax_highlighting="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 [[ -f $zsh_syntax_highlighting ]] && source "$zsh_syntax_highlighting"
 
-# Zsh "plugin" installed via git and the following command:
-# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+## Zsh "plugin" installed via git and the following command:
+## git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 zsh_autosuggestions="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
 [[ -f $zsh_autosuggestions ]] && source "$zsh_autosuggestions"
 
-# Zsh "plugin" installed via git and the following command:
-# git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+## Zsh "plugin" installed via git and the following command:
+## git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
 fzf_tab="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab/fzf-tab.plugin.zsh"
 [[ -f $fzf_tab ]] && hash fzf 2>/dev/null && source "$fzf_tab"
 
@@ -201,14 +188,22 @@ zstyle ':completion:*:descriptions' format '[%d]'
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 ## Preview directory's content with `eza` when completing `cd`.
 hash eza 2>/dev/null \
-&& zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+    && zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
 # Switch group using `<` and `>`.
 zstyle ':fzf-tab:*' switch-group '<' '>'
+
+
+####[[ End of File Configurations ]]####################################################
+#### These are configurations that are specified to be placed at the end of the file, by
+#### the developer/documentation.
+
+
+# Initialize Starship prompt, if it is installed and $ZSH_THEME is not set.
+hash starship 2>/dev/null \
+    && [[ -z $ZSH_THEME ]] \
+    && eval "$(starship init zsh)"
 
 
 ####[[ Others ]]########################################################################
 #### These are generally configurations set up by setup scripts or other programs.
 
-
-# CodeWhisperer post block. Keep at the bottom of this file.
-[[ -f "${HOME}/Library/Application Support/codewhisperer/shell/zshrc.post.zsh" ]] && builtin source "${HOME}/Library/Application Support/codewhisperer/shell/zshrc.post.zsh"
