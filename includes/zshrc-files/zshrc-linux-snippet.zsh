2fe88811c27e6f68aeb7df9365f06b39535ac8c9
Hunter T. <strangeranger@hthompson.dev>
2024-11-18 14:26:05 -0800

    Add missing code to enable fielname color

--- includes/zshrc-files/zshrc-linux-snippet.zsh
+++ includes/zshrc-files/zshrc-linux-snippet.zsh
@@ -69,4 +69,6 @@ ugit     - ugit helps you undo git commands without much effort.
 # Modifies the colors of files and directories in the terminal.
 export LS_COLORS="di=34:ln=36:so=35:pi=1;33:ex=32:bd=34;46:cd=1;33;40:su=30;41:sg=30;46:tw=30;42:ow=30;1;43"
 
+# Set list-colors to enable filename colorizing.
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 # --8<-- [end:ls_colors]
