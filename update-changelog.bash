71c0ef81e3a1f75739aab0271e5c78b09beb0a78
Hunter T. <strangeranger@hthompson.dev>
2024-12-20 16:14:58 -0800

    Final fixes and improvements before v1.0.0 release

--- update-changelog.bash
+++ update-changelog.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
 #
-# ...
+# Automate the process of updating the CHANGELOG.md file, based on the latest commit
+# messages from the dotfiles submodule.
 #
-# Version: v1.0.0-rc.1
+# Version: v1.0.0
 # License: MIT License
 #          Copyright (c) 2024 Hunter T. (StrangeRanger)
 #
@@ -10,10 +11,23 @@
 ####[ Global Variables ]################################################################
 
 
-C_CHANGELOG="CHANGELOG.md"
+readonly C_CHANGELOG="CHANGELOG.md"
+readonly C_TMP_CHANGELOG="CHANGELOG.tmp"
+readonly C_SUBMODULE_PATH="submodules/dotfiles"
+
 C_DATE=$(date +%Y-%m-%d)
-C_TMP_CHANGELOG="CHANGELOG.tmp"
-readonly C_CHANGELOG C_DATE C_TMP_CHANGELOG
+readonly C_DATE
+
+## ANSI color codes.
+C_BLUE="$(printf '\033[0;34m')"
+C_RED="$(printf '\033[1;31m')"
+C_NC="$(printf '\033[0m')"
+readonly C_BLUE C_RED C_NC
+
+## Shorthanded variables for colorized output.
+readonly C_ERROR="${C_RED}ERROR:${C_NC} "
+readonly C_INFO="${C_BLUE}==>${C_NC} "
+
 
 declare -A sections
 
@@ -21,15 +35,42 @@ declare -A sections
 ####[ Main ]############################################################################
 
 
-## Extract latest commit messages from submodule.
-git submodule update --remote
-cd submodules/dotfiles/ || exit 1
-COMMITS=$(git log $(git rev-parse HEAD@{1})..HEAD --pretty=format:"%s")
-cd - || exit 1
+###
+### Extract latest commit messages.
+###
 
-# Prepare new changelog entry.
-echo "## $C_DATE" > "$C_TMP_CHANGELOG"
-echo "" >> "$C_TMP_CHANGELOG"
+echo "${C_INFO}Updating submodule..."
+git submodule update --remote
+
+cd "$C_SUBMODULE_PATH" || {
+    echo "${C_ERROR}Failed to change directory to '$C_SUBMODULE_PATH'"
+    exit 1
+}
+
+echo "${C_INFO}Fetching latest commits..."
+C_COMMITS=$(git log "$(git rev-parse HEAD@"{1}")..HEAD" --pretty=format:"%s")
+
+if [[ -z $C_COMMITS ]]; then
+    echo "${C_ERROR}Could not determine previous commit(s)"
+    exit 1
+fi
+
+cd - || {
+    echo "${C_ERROR}Failed to change directory back to project's root directory"
+    exit 1
+}
+
+echo "${C_INFO}Prepping new changelog entry..."
+{
+    echo "## $C_DATE"
+    echo ""
+} > "$C_TMP_CHANGELOG"
+
+###
+### Parse commit messages and append to the appropriate section.
+###
+
+echo "${C_INFO}Parsing commit messages..."
 
 while IFS= read -r commit; do
     skip=false
@@ -45,14 +86,10 @@ while IFS= read -r commit; do
         echo "Info: $info"
         echo "Message: $message"
 
-        # Define keywords to skip based on type.
-        case "$type" in
-            Added|Changed|Removed|Fixed)
-                if [[ "$info" == "(chezmoi)" || "$info" == "(dotfiles)" ]]; then
-                    skip=true
-                fi
-                ;;
-        esac
+        ## Skip commits that only affect the dotfiles repository.
+        if [[ "$info" == "(chezmoi)" || "$info" == "(dotfiles)" ]]; then
+            skip=true
+        fi
     fi
 
     if [ "$skip" = true ]; then
@@ -60,18 +97,24 @@ while IFS= read -r commit; do
         continue
     fi
 
-    # Append to the appropriate section.
+    # Append commit to the appropriate section.
     sections["$type"]+="- $commit"$'\n'
     echo "---"  # Debug separator.
-done <<< "$COMMITS"
+done <<< "$C_COMMITS"
 
+###
+### Add new entries to the changelog.
+###
+
+echo "${C_INFO}Updating the changelog..."
 for type in "${!sections[@]}"; do
-    echo "### $type" >> "$C_TMP_CHANGELOG"
-    echo "" >> "$C_TMP_CHANGELOG"
-    echo "${sections[$type]}" >> "$C_TMP_CHANGELOG"
+    {
+        echo "### $type"
+        echo ""
+        echo "${sections[$type]}"
+    } >> "$C_TMP_CHANGELOG"
 done
 
-# Insert the new entry under ## Unreleased
 awk -v new_entry="$(cat $C_TMP_CHANGELOG)" '
    /^## Unreleased$/ {
        print;
@@ -82,5 +125,7 @@ awk -v new_entry="$(cat $C_TMP_CHANGELOG)" '
    { print }
 ' "$C_CHANGELOG" > "${C_CHANGELOG}.tmp"
 
-rm "$C_TMP_CHANGELOG"
 mv "${C_CHANGELOG}.tmp" "$C_CHANGELOG"
+
+echo "${C_INFO}Cleaning up..."
+rm "$C_TMP_CHANGELOG"
